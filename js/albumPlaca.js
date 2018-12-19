4fd42d5a40b1daf1821956ae1a81a535ffe8f451
marquimedes <43608704+marquimedes@users.noreply.github.com>
2018-12-19 21:15:07 +0100

    Add files via upload

---
+++ js/albumPlaca.js
@@ -0,0 +1,166 @@
+//Per canviar les fotos que mostra la galeria, només cal editar el vector "imgs" de GALERIA i el numero del "for"
+
+//Poden funcionar les fletxes per passar fotots??? i Esc per sortir de diapositiva??
+
+/*GALERIA*/{
+	var imgs = [
+		"img/albums/albumPlaca/01.jpg",
+		"img/albums/albumPlaca/02.jpg",
+		"img/albums/albumPlaca/03.jpg",
+		"img/albums/albumPlaca/04.jpg",
+		"img/albums/albumPlaca/05.jpg",
+		"img/albums/albumPlaca/06.jpg",
+		"img/albums/albumPlaca/07.jpg",
+		"img/albums/albumPlaca/08.jpg",
+		"img/albums/albumPlaca/09.jpg"
+		]
+	for(j=10; j<38; j++){
+		imgs.push("img/albums/albumPlaca/" + j + ".jpg");}
+	
+	var cont = document.getElementsByClassName('contingut')[0];
+	
+	for(var j=0; j<imgs.length ; j++){
+		var quad = document.createElement('div');
+		quad.className = 'quadricula';
+		var img = document.createElement('img');
+		quad.appendChild(img);
+		cont.appendChild(quad);
+		img.src = imgs[j];
+	}
+			/*Hem escrit:{
+			<div class ="contingut">
+				<div class="quadricula">
+					<img src="..." onclick="obraVista(); selecDiap(0);">
+				</div>
+				...
+				<div class="quadricula">
+					<img src="..." onclick="obraVista(); selecDiap(50);">
+				</div>
+			</div>
+			}*/
+}
+
+
+/*VISTA GRAN*/
+
+	/*Escrivim el necesari en l'arxiu HTML*/{
+	// Utilitza el vector "imgs" de l'apartat "GALERIA"
+	
+	for(var j=0; j<imgs.length ; j++){
+		var img = document.getElementsByClassName('quadricula')[j].children[0];
+		img.setAttribute( "onClick", "obraVista(); selecDiap("+j+");");
+	}
+	
+	var vistag = document.getElementsByClassName('vistagran')[0];
+	for(var j=0; j<imgs.length ; j++){
+		var diap = document.createElement('div');
+		diap.className = 'diapositiva';
+		var diapnum = document.createElement('div');
+		diapnum.className = 'diapnumero';
+		var img = document.createElement('img');
+		diap.appendChild(diapnum);
+		diap.appendChild(img);
+		vistag.appendChild(diap);
+		
+		img.src = imgs[j];
+		img.style='width:100%';
+		document.getElementsByClassName('diapnumero')[j].innerHTML = (j+1)+"/"+imgs.length;
+	}
+	
+	var vista = document.getElementsByClassName('vista')[0];
+	var ant = document.createElement('a');
+	var seg = document.createElement('a');
+	ant.className = 'anterior';
+	seg.className = 'seguent';
+	vista.appendChild(ant);
+	vista.appendChild(seg);
+	document.getElementsByClassName('anterior')[0].innerHTML = "";
+	document.getElementsByClassName('seguent')[0].innerHTML = "";
+	ant.setAttribute( "onClick", 'mesDiaps(-1)');
+	seg.setAttribute( "onClick", 'mesDiaps(1)');
+	
+	var carr = document.getElementsByClassName('carret')[0];
+	for(var j=0; j<imgs.length ; j++){
+		var miniat = document.createElement('div');
+		miniat.className = 'miniatura';
+		var img = document.createElement('img');
+		miniat.appendChild(img);
+		carr.appendChild(miniat);
+		img.className = 'demo';
+		img.src = imgs[j];
+		img.setAttribute( "onClick", "selecDiap("+j+");");
+	}
+			/*Hem escrit:{
+			<div id="vistadiapositiva" class="vista" style="displai:none">
+				
+				<img src="C:/PC/MARC/WEB/img/inici/close.png" class="close" onclick="tancaVista()">
+				
+				<div class="vistagran">
+					<div class="diapositiva" style="display:none">
+						<div class="diapnumero">1/51</div>
+						<img src="..." style="width:100%">
+					</div>
+					...
+					<div class="diapositiva" style="display:none">
+						<div class="diapnumero">51/51</div>
+						<img src="..." style="width:100%">
+					</div>
+				</div>
+				
+				<a class="anterior" onclick="mesDiaps(-1)"></a>
+				<a class="seguent" onclick="mesDiaps(1)"></a>
+				
+				<div class="carret">
+					<div class="miniatura">
+						<img class="demo" src="..." onclick="selecDiap(0)>
+					</div>
+					...
+					<div class="miniatura">
+						<img class="demo" src="..." onclick="selecDiap(50)>
+					</div>
+				</div>
+			</div>
+			}*/
+}
+	
+	/*Interacció*/{
+		
+	function obraVista(){
+		document.getElementById('vistadiapositiva').style.display = "block";
+	}
+	function tancaVista(){
+		document.getElementById('vistadiapositiva').style.display = "none";
+	}
+
+	var diapIndex = 0;
+	
+	function mesDiaps(n){
+		mostraDiaps(diapIndex += n);
+	}
+	function selecDiap(n){
+		mostraDiaps(diapIndex = n);
+	}
+
+	function mostraDiaps(n){
+		var k;
+		var diaps = document.getElementsByClassName("diapositiva");
+		var dem = document.getElementsByClassName("miniatura");
+		if(n>=diaps.length) {diapIndex = 0}
+		if(n<0) {diapIndex = diaps.length - 1}
+		for(k=0; k<diaps.length ; k++){
+			diaps[k].style.display = "none";
+		}
+		for(k=0; k<dem.length ; k++){
+			dem[k].children[0].className = "demo";
+		}
+		diaps[diapIndex].style.display = "block";
+		dem[diapIndex].children[0].className = "active";
+		
+		//AUTO-SCROLL
+		var scrollH = document.getElementsByClassName('carret')[0].scrollHeight;
+		var scrollP = diapIndex/(dem.length-1);// = On definirem scroll Position, entre 0 i 1
+		var scrollI = window.innerHeight;
+		scrollI = scrollI/2;// = scroll Increment
+		document.getElementsByClassName('carret')[0].scrollTo(0, (scrollP*scrollH)-scrollI+130 );
+	}
+}
---
+++ js/menuDesp.js
@@ -0,0 +1,119 @@
+//ES REQUEREIX L'ETIQUETA <div class="menuDesp"> EN EL BODY DE L'HTML
+
+/*/Codi HTML:*/{
+var menu = document.getElementsByClassName('menuDesp')[0];
+//menuIc
+var icona = document.createElement('img');
+icona.className = 'menuIc';
+menu.appendChild(icona);
+icona.src = "img/icones/menu.png";
+//menuLat
+var lateral = document.createElement('div');
+lateral.className = 'menuLat';
+lateral.style.display = "none";
+menu.appendChild(lateral);
+	//Titol
+	var titol = document.createElement('div');
+	titol.className = 'menuTit';
+	lateral.appendChild(titol);
+	titol.innerHTML = "<a href='inici.html'><h1>M.A.</h1><h3>FOTOGRAFIA</h3></a>";
+	//Contenedor apartats
+	var apartats = document.createElement('div');
+	apartats.className = 'menuAps';
+	lateral.appendChild(apartats);
+		//Apartat INICI
+		var apartat = document.createElement('div');
+		apartat.className = 'menuAp';
+		apartats.appendChild(apartat);
+		apartat.innerHTML = "<a href='inici.html'>INICI</a>";
+		//Espai
+		var espai = document.createElement('div');
+		espai.className = 'menuEsp';
+		apartats.appendChild(espai);
+		//Espai
+		var espai = document.createElement('div');
+		espai.className = 'menuEsp';
+		apartats.appendChild(espai);
+		//Apartat PORTFOLI
+		var apartat = document.createElement('div');
+		apartat.className = 'menuAp';
+		apartats.appendChild(apartat);
+		apartat.innerHTML = "<a href='portfoli.html'>PORTFOLI</a>";
+		//Espai
+		var espai = document.createElement('div');
+		espai.className = 'menuEsp';
+		apartats.appendChild(espai);
+		//Apartat ÀLBUMS
+		var apartat = document.createElement('div');
+		apartat.className = 'menuAp';
+		apartats.appendChild(apartat);
+		apartat.innerHTML = "<a href='albums.html'>ÀLBUMS</a>";
+		//Espai
+		var espai = document.createElement('div');
+		espai.className = 'menuEsp';
+		apartats.appendChild(espai);
+		//Apartat INSTSAGRAM + PERFIL
+		var apartat2 = document.createElement('div');
+		apartat2.className = 'menuAp2';
+		apartats.appendChild(apartat2);
+		apartat2.innerHTML = "<a href='https://www.instagram.com/marc.alma/?hl=es' target='_blank'>" +
+						"<img src='img/icones/instagram.png'> </a>" +
+						"<a href='#'><img src='img/icones/profile.png'></a>";
+
+//Fons
+var mBack = document.createElement('div');
+mBack.className = 'menuBack';
+mBack.style.display = "none";
+menu.appendChild(mBack);
+
+//Subapartats a CRONIQUES
+var croniq = document.getElementsByClassName('menuAp')[2];
+var subapVec = ["Ruta al Mont Perdut",
+				"Cala Jòncols, Primavera",
+				"Tromso, Noruega",
+				"Correfoc, la Mercè '18",
+				"Escalada a la placa Gandhi",
+				];
+var contenedorSubaps = document.createElement('div');
+croniq.appendChild(contenedorSubaps);
+contenedorSubaps.className = 'menuSubaps'
+for(j=0; j<subapVec.length; j++){
+	var subap = document.createElement('a');
+	contenedorSubaps.appendChild(subap);
+	subap.innerHTML = "<p>" + subapVec[j] + "</p>";
+}
+document.getElementsByClassName("menuSubaps")[0].children[0].href = "albumMontperdut.html"
+document.getElementsByClassName("menuSubaps")[0].children[1].href = "albumCalajoncols.html"
+document.getElementsByClassName("menuSubaps")[0].children[2].href = "albumTromso.html"
+document.getElementsByClassName("menuSubaps")[0].children[3].href = "albumCorrefoc.html"
+document.getElementsByClassName("menuSubaps")[0].children[4].href = "albumPlaca.html"
+//document.getElementsByClassName("menuSubaps")[0].children[ X ].href = "nomAlbum.html"
+}
+
+//Funció amagar i mostrar menuLat
+icona.setAttribute( "onClick", "mostra()");
+function mostra(){
+	lateral.id = "entra";
+	lateral.style.left = "0px";
+	lateral.style.display = "block";
+	mBack.style.display = "block";
+}
+mBack.setAttribute( "onClick", "amaga()");
+function amaga(){
+	lateral.id = "marxa";
+	lateral.style.left = "-400px";
+	mBack.style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
